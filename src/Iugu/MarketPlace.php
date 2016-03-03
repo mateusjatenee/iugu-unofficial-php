a78248a25f38de34ede0c67dd068d9a4afecaa26
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-02 23:30:03 -0300

    doc block

--- src/Iugu/MarketPlace.php
+++ src/Iugu/MarketPlace.php
@@ -6,21 +6,37 @@ use mateusjatenee\Iugu\Base;
 
 class MarketPlace extends Base
 {
+    /**
+     * @param array $data
+     * @return array
+     */
     public function createSubAccount($data)
     {
         return $this->post('marketplace/create_account', $data);
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function requestSubAccountVerification($id)
     {
         return $this->post('accounts/' . $id . '/request_verification');
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function fetchSubAccountInformation($id)
     {
         return $this->get('accounts/' . $id);
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function requestSubAccountWithdraw($id)
     {
         return $this->post('accounts' . $id . '/request_withdraw');
