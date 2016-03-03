f3e3037582b84103cd64cc795903ef32e9bb911d
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-02 23:00:13 -0300

    docblock

--- src/Iugu/Charge.php
+++ src/Iugu/Charge.php
@@ -2,10 +2,20 @@
 
 namespace mateusjatenee\Iugu;
 
-use mateusjatenee\Base;
+use mateusjatenee\Iugu\Base;
 
 class Charge extends Base
 {
+    /**
+     * Charges a customer
+     * Transação direta a um usuário
+     *
+     * Charges a customer. It needs the token parameter which can be gathered using iugu.js
+     * Gera uma cobrança direta a um usuário. Necessita do envio de um token que pode ser capturado pelo iugu.js
+     *
+     * @param array $data
+     * @return array
+     */
     public function chargeCustomer($data)
     {
         return $this->get('charge', $data);
