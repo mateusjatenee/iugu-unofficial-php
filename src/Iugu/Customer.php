b1e192426020b592d8186c1d120be56df7d7714e
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-02 23:16:34 -0300

    Docblock

--- src/Iugu/Customer.php
+++ src/Iugu/Customer.php
@@ -7,29 +7,39 @@ use mateusjatenee\Iugu\Base;
 class Customer extends Base
 {
 
+    /**
+     * Returns all the clients
+     * Retorna todos os clientes
+     *
+     * @return array
+     */
     public function listClients()
     {
         return $this->get('customers');
     }
 
+    /**
+     * Creates a customer
+     * Cria um cliente
+     *
+     * @param array $data
+     * @return array
+     */
     public function createCustomer($data)
     {
         return $this->post('customers', $data);
     }
 
+    /**
+     * Fetch a singular client based on his ID
+     * Retorna um cliente baseado no ID dele
+     *
+     * @param string $id
+     * @return array
+     */
     public function fetchClient($id)
     {
         return $this->get('customers/' . $id);
-    }
-
-    public function changeClient($id, $obj)
-    {
-    	return $this->put('customers/' . $id, $obj);
-    }
-
-    public function deleteClient($id);
-    {
-    	return $this->delete('customers/' . $id);
     }
 
 }
