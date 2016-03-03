c11e4cb2329234a6a54e39e19e3b08f8d6f82241
Mateus Guimarães <mateus.jatene@gmail.com>
2016-03-02 23:23:43 -0300

    adds docblock

--- src/Iugu/Invoice.php
+++ src/Iugu/Invoice.php
@@ -6,21 +6,37 @@ use mateusjatenee\Iugu\Base;
 
 class Invoice extends Base
 {
+    /**
+     * @param array $data
+     * @return array
+     */
     public function createInvoice($data)
     {
         return $this->post('invoices', $data);
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function fetchInvoice($id)
     {
         return $this->get('invoices/' . $id);
     }
 
+    /**
+     * @param string $id
+     * @param array $data
+     * @return array
+     */
     public function changeInvoice($id, $data)
     {
         return $this->put('invoices' . $id, $data);
     }
 
+    /**
+     * @return array
+     */
     public function listInvoices()
     {
         return $this->get('invoices');
